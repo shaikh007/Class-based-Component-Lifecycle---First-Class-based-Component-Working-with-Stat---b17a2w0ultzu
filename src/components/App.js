e8037b8bb7e50347a7d823f7667cab4d46dc53b1
Shaikh Irfan <shaikhji1998@gmail.com>
2022-12-08 16:38:38 +0530

    new Commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,39 +1,23 @@
 import React, { useEffect } from 'react'
-
 import '../styles/App.css';
 
+
 const App = () => {
-
   const [date,setDate] = React.useState()
-
   useEffect(()=>{
-
     setDate(new Date().toLocaleString())
-
     const timerId = setInterval(()=>{
-
       setDate(new Date().toLocaleString())
-
     },1000)
-
     return () => clearInterval(timerId)
-
   },[])
 
   return (
-
     <div id="main">
-
-      <div className="date">{date}</div>
-
+      <h1>Welcome to Newton School</h1>
+      <div className="date" id='timer'>{date}</div>
     </div>
-
   )
-
 }
-
-
-
- 
 
 export default App;
